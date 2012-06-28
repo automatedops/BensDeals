816b828c46c121326b5d715a8b3c32b4025b4c8a
Wei Wu <allminewuwei@gmail.com>
2012-06-27 23:30:06 -0700

    test

---
+++ test/java/net/bensdeals/support/TestCase.java
@@ -0,0 +1,7 @@
+package net.bensdeals.support;
+
+import android.view.View;
+
+public abstract class TestCase {
+    protected abstract View findViewById(int viewId);
+}
--- test/java/net/bensdeals/views/DealItemViewTest.java
+++ test/java/net/bensdeals/views/DealItemViewTest.java
@@ -1,11 +1,15 @@
 package net.bensdeals.views;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.inject.Inject;
 import net.bensdeals.R;
 import net.bensdeals.model.Deal;
 import net.bensdeals.support.RobolectricTestRunnerWithInjection;
+import net.bensdeals.support.TestCase;
 import net.bensdeals.util.TestImageLoader;
 import net.bensdeals.utils.LayoutInflaterWithInjection;
 import org.junit.Before;
@@ -13,9 +17,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.pivotallabs.robolectricgem.expect.Expect.expect;
+import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunnerWithInjection.class)
-public class DealItemViewTest {
+public class DealItemViewTest extends TestCase {
     @Inject LayoutInflaterWithInjection<DealItemView> inflater;
     @Inject TestImageLoader imageLoader;
     public DealItemView dealItemView;
@@ -23,14 +28,21 @@ public class DealItemViewTest {
     private TextView titleText;
     private ImageView imageView;
     private TextView descText;
+    private View shareButton;
 
     @Before
     public void setup() throws Exception {
         dealItemView = inflater.inflate(R.layout.deal_item_layout);
         deal = new Deal().setDescription("deal desc").setLink("some/web/link").setTitle("deal title").setImageUrl("some/deal/image");
-        titleText = (TextView) dealItemView.findViewById(R.id.title_text);
-        imageView = (ImageView) dealItemView.findViewById(R.id.gallery_image);
-        descText = (TextView) dealItemView.findViewById(R.id.desc_text);
+        titleText = (TextView) findViewById(R.id.title_text);
+        imageView = (ImageView) findViewById(R.id.gallery_image);
+        descText = (TextView) findViewById(R.id.desc_text);
+        shareButton = findViewById(R.id.share_button);
+    }
+
+    @Override
+    protected View findViewById(int viewId) {
+        return dealItemView.findViewById(viewId);
     }
 
     @Test
@@ -39,5 +51,22 @@ public class DealItemViewTest {
         expect(titleText).toHaveText("deal title");
         expect(descText).toHaveText("deal desc");
         expect(imageLoader.loadedImageUrl(imageView)).toEqual("some/deal/image");
+    }
+
+    @Test
+    public void shouldHaveShareClickListener() throws Exception {
+        dealItemView.render(deal);
+        expect(shadowOf(shareButton).getOnClickListener()).not.toBeNull();
+    }
+
+    @Test
+    public void shouldLaunchShareIntent() throws Exception {
+        dealItemView.render(deal);
+        shareButton.performClick();
+        Intent intent = shadowOf(new Activity()).peekNextStartedActivity();
+        expect(intent.getAction()).toEqual(Intent.ACTION_CHOOSER);
+        Intent parcelableExtra = (Intent) intent.getParcelableExtra(Intent.EXTRA_INTENT);
+        expect(parcelableExtra.getStringExtra(Intent.EXTRA_SUBJECT)).toEqual("deal title");
+        expect(parcelableExtra.getCharSequenceExtra(Intent.EXTRA_TEXT)).toEqual("deal desc");
     }
 }
