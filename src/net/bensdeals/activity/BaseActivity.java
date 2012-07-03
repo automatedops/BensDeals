29e460978aa695509dc10569357761847502d770
Wei Wu <allminewuwei@gmail.com>
2012-07-03 16:59:58 -0700

    add flurry

--- src/net/bensdeals/activity/BaseActivity.java
+++ src/net/bensdeals/activity/BaseActivity.java
@@ -2,7 +2,9 @@ package net.bensdeals.activity;
 
 import android.os.Bundle;
 import android.view.Window;
+import com.flurry.android.FlurryAgent;
 import com.google.inject.Inject;
+import net.bensdeals.R;
 import net.bensdeals.network.ImageLoader;
 import roboguice.activity.RoboActivity;
 
@@ -13,11 +15,18 @@ public abstract class BaseActivity extends RoboActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        FlurryAgent.onStartSession(this, getString(R.string.flurry_id));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         imageLoader.cancelOutstandingRequests();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FlurryAgent.onEndSession(this);
     }
 }
